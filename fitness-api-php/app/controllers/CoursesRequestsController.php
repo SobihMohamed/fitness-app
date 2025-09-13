66e0cc7babdfb8233870c0a945ce31ed61e1dc91
Sobih <sobihmohamedsobih@gmail.com>
2025-09-13 18:49:30 +0300

    End functionality of the Backend and end of the promo code handeled

--- fitness-api-php/app/controllers/CoursesRequestsController.php
+++ fitness-api-php/app/controllers/CoursesRequestsController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controllers;
 use App\Core\AbstractController;
+use App\models\Courses;
 use App\models\CoursesRequest;
+use App\models\Promo_Codes;
 use App\Helpers\NotifyHelper;
+
+use DateTime;
 class CoursesRequestsController extends AbstractController {
   protected $reqModel;
 
@@ -11,7 +15,7 @@ class CoursesRequestsController extends AbstractController {
     $this->reqModel = new CoursesRequest();
   }
 
-   // POST /user/requests  (submit new) required user_id in hidden and all data
+  // POST /user/requests  (submit new) required user_id in hidden and all data
   public function create(){
     $user = $this->getUserFromToken();
     if ($_SERVER['REQUEST_METHOD']!=='POST')
@@ -19,15 +23,40 @@ class CoursesRequestsController extends AbstractController {
 
     $data = json_decode(file_get_contents("php://input"), true);
     $data['user_id'] = $user['id'];
-
-// تأكد أن course_id مرسل
+    // تأكد أن course_id مرسل
     if (empty($data['course_id']))
         return $this->sendError("Course ID is required", 400);
-
-    // احضر اسم الكورس
-    $course = $this->reqModel->getSpecificRequestById($data['course_id']);
+    $courseModel = new Courses();
+    $course = $courseModel->getCourseById($data['course_id']);
+    
     if (!$course)
         return $this->sendError("Course not found", 404);
+
+    $data['original_total'] = $course['price'];
+    if($data['promo_code_used'] != null && trim($data['promo_code_used']) != "") {
+        $promoModel = new Promo_Codes();
+        $PromoCode = $promoModel->getPromoByPromo($data['promo_code_used']);
+        if(empty($PromoCode)){
+          $this->sendError("InValid Promo Code");
+          return;
+        }
+        $currentDate = new DateTime();
+        $endDate = new DateTime($PromoCode['end_date']);
+        $percentageOfDiscount = 0;
+        if(!empty($PromoCode)){
+          if($currentDate < $endDate){
+            $percentageOfDiscount = $PromoCode['percentage_of_discount'];
+            $data['discount_value'] = $percentageOfDiscount;
+            $data['net_total'] = $data['original_total'] - ($data['original_total'] * $percentageOfDiscount / 100) ;
+          }else{
+            $this->sendError("Promo Code Not Available");
+            return;
+          }
+        }
+    }
+
+
+    // احضر اسم الكورس
 
     $ok = $this->reqModel->create($data);
     if (!$ok) 
@@ -36,7 +65,7 @@ class CoursesRequestsController extends AbstractController {
     // إشعار لكل الأدمنات
     NotifyHelper::pushToAllAdmins(
         "طلب اشتراك في كورس جديد",
-        "قام المستخدم {$user['email']} بطلب الاشتراك في الكورس: {$course['name']} (ID: {$data['course_id']})"
+        "قام المستخدم {$user['email']} بطلب الاشتراك في الكورس: {$course['title']} (ID: {$data['course_id']})"
     );
 
     // إشعار للمستخدم
@@ -45,7 +74,7 @@ class CoursesRequestsController extends AbstractController {
         "تم استلام طلبك",
         "تم استلام طلب الاشتراك في الكورس بنجاح، وسيتم مراجعته قريبًا."
     );
-                                
+    
     return $this->json(["status"=>"success","message"=>"Request submitted"]);
   }
   //get requests sent
